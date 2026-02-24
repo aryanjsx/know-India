88b2c8fbde4146e5b1c2cef401a31d7eeb815a03
Aryan Kumar <57598770+aryanjsx@users.noreply.github.com>
2026-02-24 22:40:35 +0530

    Change heading

--- frontend/src/pages/constitution.jsx
+++ frontend/src/pages/constitution.jsx
@@ -567,7 +567,7 @@ const Constitution = () => {
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-3 text-amber-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
               </svg>
-              Fun Facts About Our Constitution
+              Facts About Our Constitution
               </h3>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                 {[
